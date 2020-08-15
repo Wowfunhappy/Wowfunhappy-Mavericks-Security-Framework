5f452917c8e20a95494d07f9059dfda08525090c
Jonathan Alland <Jonathan@Jonathans-Mac.local>
2020-08-15 14:34:41 -0700

    Add to esp.h

--- Headers/esp.h
+++ Headers/esp.h
@@ -7,4 +7,8 @@
 int __esp_enabled();
 int __esp_check(char *key, xpc_object_t dict);
 
+//From https://github.com/darlinghq/darling-security/blob/0db0acec470cea7b66bfd75838e0c136d20a286c/include/esp.h
+#define __esp_check_ns(a, b) -1
+#define __esp_notify_ns(a, b)
+
 #endif
